2e4801e128f1eb3c406a11563ecd37c066211851
Raymond Metz <km0852555@otc.edu>
2018-11-05 21:04:10 -0600

    Edited index.js file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const jsonfile = require('jsonfile');
-const file = 'data.json';
+var jsonfile = require('jsonfile');
+var _ = require('lodash');
+var file = 'data.json';
 jsonfile.readFile(file, function(err, obj)
 {
-    if(err)
-    {
-        console.error(err);
-    }
-    console.dir(obj);
+    _.forEach(obj, value =>
+        {
+            console.log(value);
+        });
+
 });

5557abca7ea2e764ed2e7961e8fbacb25d457816
Raymond Metz <km0852555@otc.edu>
2018-11-05 20:53:04 -0600

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonfile = require('jsonfile');
+const file = 'data.json';
+jsonfile.readFile(file, function(err, obj)
+{
+    if(err)
+    {
+        console.error(err);
+    }
+    console.dir(obj);
+})
